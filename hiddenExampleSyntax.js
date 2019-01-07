9f1f8c4832f82951d9e82ac54b8496e261ff4799
Mark S. Miller <erights@gmail.com>
2019-01-06 16:28:36 -0800

    .

--- hiddenExampleSyntax.js
+++ hiddenExampleSyntax.js
@@ -1,11 +1,11 @@
 /*global require*/
 // Original syntax example from
-// https://github.com/zenparsing/hidden-state/blob/master/README.md
-// but with gratutous differences from
 /*
 https://github.com/tc39/proposal-class-fields/issues/183#issuecomment-451733300
 */
-// repaired.
+
+
+
 
 const { hiddenState } = require('./hidden-state');
 
@@ -14,21 +14,21 @@ const [getState, initState, isInstance] = hiddenState('Point');
 class Point {
   
   constructor(x, y) {
-    initState(this, { x, y });
+    this->initState({ x, y });
   }
 
   toString() {
-    let { x, y } = getState(this);
+    let { x, y } = this->getState();
     return `<${x}:${y}>`;
   }
 
   add(p) {
-    let { x, y } = getState(this);
-    let { x: x1, y: y1 } = getState(p);
+    let { x, y } = this->getState();
+    let { x: x1, y: y1 } = p->getState();
     return new Point(x + x1, y + y1);
   }
 
   static isPoint(x) {
-    return isInstance(x);
+    return x->isInstance();
   }
 }
